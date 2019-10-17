90ffc7981d233eaf855050ec3e69c9cfce085b10
João Teixeira <joao@ycloud.com.br>
2019-10-17 11:16:10 -0300

    Adicionado RolesAndPermissionsSeeder

---
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit quotations']);
+        Permission::create(['name' => 'delete quotations']);
+        Permission::create(['name' => 'publish quotations']);
+
+        // create roles and assign created permissions
+
+        // this can be done as separate statements
+        $role = Role::create(['name' => 'publisher']);
+        $role->givePermissionTo('edit quotations');
+
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
