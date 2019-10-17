e883dd5bd5f562bff484f68171a287a6df71a30c
João Teixeira <joao@ycloud.com.br>
2019-10-17 10:26:33 -0300

    Instalação e configuração do pacote spatie/laravel-permission

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
